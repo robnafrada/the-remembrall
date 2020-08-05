0a264eec61ff03b48cea72617c35060f0f807ba0
Robert Nafrada <robnafrada@gmail.com>
2020-08-04 23:01:21 -0400

    added audio files

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 class AudioController {
     constructor() {
         this.bgMusic = new Audio('assets/audio/hedwigTheme.mp3');
+        this.victoryMusic = new Audio('assets/audio/victory.mp3');
         this.bgMusic.volume = 0.5;
         this.bgMusic.loop = true;
     }
     startMusic() {
         this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    victory() {
+        this.stopMusic();
+        this.victoryMusic.play();
     }
 }
 
